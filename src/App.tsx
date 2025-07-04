e350926a9d9cd9b20979ab8b629e6644c97dc812
KotSerhii <kot.serhii@gmail.com>
2025-07-04 15:47:10 +0300

    Commit lesson2 useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import {useState} from 'react';
 function App() {
+    let [counter, setCounter] = useState<number>(0);
 
     return (
         <>
-
+            <h2>{counter}</h2>
+            <button onClick={()=>{
+                setCounter(++counter);
+            }}>increment</button>
+            <button onClick={()=>{
+                setCounter((prevState:number)=>{return --prevState});
+            }}>decrement</button>
         </>
     )
 }
