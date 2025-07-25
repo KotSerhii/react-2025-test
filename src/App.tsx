85dc8cb935417cd3fbe4ef8f909dbca0dcc0d7e7
KotSerhii <kot.serhii@gmail.com>
2025-07-25 19:50:07 +0300

    Commit lesson2-2 useState add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
-import {useState}  from 'react';
+import {useState} from 'react';
 import TimerComponent from '../src/components/timer-component/timerComponent'
+
 function App() {
     let [counter, setCounter] = useState<number>(0);
 
@@ -8,13 +9,17 @@ function App() {
         <>
             <TimerComponent/>
             <h2>{counter}</h2>
-            <button onClick={()=>{
+            <button onClick={() => {
                 setCounter(++counter);
-            }}>increment</button>
-            <button onClick={()=>{
-                setCounter((prevState:number)=>{return --prevState});
-            }}>decrement</button>
-
+            }}>increment
+            </button>
+            <button onClick={() => {
+                setCounter((prevState: number) => {
+                    return --prevState
+                });
+            }}>decrement
+            </button>
+            <UsersComponent/>
         </>
     )
 }
