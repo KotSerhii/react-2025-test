1dc715b4bef1ab4e8bd91cf8e173dac68e90d25d
KotSerhii <kot.serhii@gmail.com>
2025-09-01 13:48:24 +0300

    add Task5 complete

--- src/components/product-component/Product.tsx
+++ src/components/product-component/Product.tsx
@@ -1,17 +1,33 @@
-import type { FC } from 'react';
-import type { IProduct } from "../../models/Product";
+import {useState, type FC} from 'react';
+import type {IProduct} from "../../models/Product";
 
 type ProductPropsType = {
     product: IProduct;
 }
 
 const Product: FC<ProductPropsType> = ({product}) => {
+    const [showDetails, setShowDetails] = useState<boolean>(false);
     return (
         <div className="border rounded-xl p-4 shadow-md max-w-sm">
-           <img src={product.thumbnail} alt={product.title}  className='w-full h-48 object-cover rounded-lg'/>
+            <img src={product.thumbnail} alt={product.title} className='w-full h-48 object-cover rounded-lg'/>
             <h2 className="text-xl font-bold mt-2">{product.title}</h2>
             <p className="text-gray-500">{product.brand}</p>
+            <p className='text-lg font-semibold mt-2'>{product.price}</p>
+            <p className="text-yellow-500">⭐{product.rating}</p>
+            <button
+                className="mt-3 px-4 py-2 bg-blue-500 text-white rounded-lg"
+                onClick={() => setShowDetails(!showDetails)}
+            >
+                {showDetails ? "Hide details" : "Show details"}
+            </button>
 
+            {showDetails && (
+                <div className="mt-3 text-sm text-gray-700">
+                    <p>{product.description}</p>
+                    <p className="mt-2">Stock: {product.stock}</p>
+                    <p>Category: {product.category}</p>
+                </div>
+            )}
         </div>
     );
 };
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,7 @@
 import type { IProduct } from "../models/Product";
 import type { IProductsResponseDummyjson } from "../models/ProductsResponseDummyjson";
 
-const endpointProducts = import.meta.env.VITE_API_BASE_URL+'/products&limit=0';
+const endpointProducts = import.meta.env.VITE_API_BASE_URL + '/products?limit=0';
 
 const loadProducts =async ():Promise<IProduct[]> => {
 const response: IProductsResponseDummyjson = await fetch(endpointProducts)
