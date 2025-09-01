8b51f4d1b6b2b4795329f370e91fcbcb86f2e0af
KotSerhii <kot.serhii@gmail.com>
2025-09-01 13:50:08 +0300

    add Task5 complete add margin

--- src/components/product-component/Product.tsx
+++ src/components/product-component/Product.tsx
@@ -8,7 +8,7 @@ type ProductPropsType = {
 const Product: FC<ProductPropsType> = ({product}) => {
     const [showDetails, setShowDetails] = useState<boolean>(false);
     return (
-        <div className="border rounded-xl p-4 shadow-md max-w-sm">
+        <div className="border rounded-xl m-2 p-4 shadow-md max-w-sm">
             <img src={product.thumbnail} alt={product.title} className='w-full h-48 object-cover rounded-lg'/>
             <h2 className="text-xl font-bold mt-2">{product.title}</h2>
             <p className="text-gray-500">{product.brand}</p>
