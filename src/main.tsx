4afeb5bcab584c96bfc9ce40e309be44cc8b5e03
KotSerhii <kot.serhii@gmail.com>
2025-09-10 12:46:04 +0300

    lesson3 RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,25 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider } from "react-router-dom"
+import MainLayout from "./layouts/MainLayout/MainLayout"
+import APage from "./pages/APage"
+import BPage from "./pages/BPage"
+import MainPage from "./pages/MainPage"
 
-createRoot(document.getElementById('root')!).render(<App />)
+
+const router=createBrowserRouter(
+    [
+        {
+            path:'/',element:<MainLayout/>,          //path - початкова url, MainLayout-головний основний компонент
+            children:                                //дочірні елементи, які з'являються після "/"
+            [
+                {path:'a',element:<APage/>},         // тобто path - це URL, a element - відображається в Outlet
+                {path:'b',element:<BPage/>},
+                {path:'',element:<MainPage/>},
+            ]
+        },
+
+    ]
+)
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={router}/>)
