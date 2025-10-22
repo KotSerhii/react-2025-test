af0e34a83074270973b2c801be0224b9b2dce7fd
KotSerhii <kot.serhii@gmail.com>
2025-10-22 16:11:42 +0300

    add lesson3.1 state and useNavigate() + useLocation()

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,6 +3,7 @@ import Layout from "../layouts/Layout";
 import HomePage from "../pages/HomePage";
 import PostsPage from "../pages/PostsPage";
 import UsersPage from "../pages/UsersPage";
+import SingleUserDetailsPage from "../pages/SingleUserDetailsPage";
 
 
 export const routes= createBrowserRouter([
@@ -10,6 +11,7 @@ export const routes= createBrowserRouter([
         path:"", element:<Layout/>, children:[
             {index: true, element:<HomePage/>},
             {path:"users", element:<UsersPage/>},
+            {path:"users/details", element:<SingleUserDetailsPage/>},
             {path: 'posts', element: <PostsPage/>}
         ]
     }
