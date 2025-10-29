db3a399ce256a8e406db9be48841be1b7238f21b
KotSerhii <kot.serhii@gmail.com>
2025-10-29 11:46:55 +0200

    HomeWork3 Task2

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,16 @@ import App from "../App";
 
 export const routes=[{
     path:"/",element:<App/>,children:[{
-        path:"/users", element:<UsersComponent/>
-    }]
-}]
+        path:"/users", element:<UsersPage/>,children:[{
+            path:"/users/jsonplaceholder", element: <UsersFromJsonPage/>},
+            {path: "/users/dummyjson", element: <UsersFromDummyPage/>
+        }]},
+        {path:"/posts", element:<PostsPage/>,children:[
+            {path:"/posts/jsonplaceholder", element: <PostsFromJsonPage/>},
+            {path: "/posts/dummyjson", element: <PostsFromDummyPage/>}
+]
+    },
+        {path:"/comments", element:<CommentsPage/>,children:[
+                {path:"/comments/jsonplaceholder", element: <CommentsFromJsonPage/>}
+    ]}
+]}]
