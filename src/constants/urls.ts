0b3a54002abb1337c4bf6caca7b8f9708e4ac4d3
KotSerhii <kot.serhii@gmail.com>
2025-11-05 15:40:55 +0200

    HomeWork3 Task2.2

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,15 +3,17 @@ const baseUrls = {
     Dummyjson: 'https://dummyjson.com'
 };
 
-export const users={
-    allUsersFromJson: baseUrls.JsonPlaceholder+'/users',
-    allUsersFromDummyJson: baseUrls.Dummyjson+'/users'
-}
-export const posts={
-    allPostsFromJson: baseUrls.JsonPlaceholder+'/posts',
-    allPostsFromDummyJson: baseUrls.Dummyjson+'/posts'
-}
-export const comments={
-    allCommentsFromJson: baseUrls.JsonPlaceholder+'/comments',
-    allCommentsFromDummyJson: baseUrls.Dummyjson+'/comments'
+export const urls = {
+    users:{
+        allUsersFromJson: baseUrls.JsonPlaceholder+'/users',
+        allUsersFromDummyJson: baseUrls.Dummyjson+'/users'
+    },
+    posts:{
+        allPostsFromJson: baseUrls.JsonPlaceholder+'/posts',
+        allPostsFromDummyJson: baseUrls.Dummyjson+'/posts'
+    },
+    comments:{
+        allCommentsFromJson: baseUrls.JsonPlaceholder+'/comments',
+        allCommentsFromDummyJson: baseUrls.Dummyjson+'/comments'
+    }
 }
