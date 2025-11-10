d401db6f500d372890055ed5ffd14edde3b41705
KotSerhii <kot.serhii@gmail.com>
2025-11-10 23:43:47 +0200

    HomeWork3 Task2.2 add Dummyjson models

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,13 +1,13 @@
-import {Link, Outlet } from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 const PostsPage = () => {
     return (
         <div>
             <ul className="menu">
-                <li><Link to={'/posts/dummyjson'}>Posts from dummyjson</Link></li>
                 <li><Link to={'/posts/jsonplaceholder'}>Posts from jsonplaceholder</Link></li>
+                <li><Link to={'/posts/dummyjson'}>Posts from dummyjson</Link></li>
             </ul>
-<hr></hr>
+            <hr></hr>
             <Outlet/>
         </div>
     );
