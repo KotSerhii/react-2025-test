56ff46599f01db3950466fd1f1033c00e3037052
KotSerhii <kot.serhii@gmail.com>
2025-11-24 01:13:32 +0200

    HomeWork3 Task3.3 complete

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,9 +6,9 @@ import CartsPage from "../pages/CartsPage";
 export const routes = createBrowserRouter([
     {
         path: "/", element: <MainLayout/>, children: [
-            {path: "users", element: <UsersPage/>, children: [
-                    {path: ":id/carts", element: <CartsPage/>}
-                ]}
+            {path: "users", element: <UsersPage/>},
+            {path: "users/:id/carts", element: <CartsPage/>}
+
 
         ]
     },
