8c31964c1159a6e8dd594039c89b08f22a2b01e0
KotSerhii <kot.serhii@gmail.com>
2025-12-02 13:40:14 +0200

    Хук useForm (handleSubmit,register)

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,43 +1,27 @@
-import {useState, type FormEvent} from "react";
 
+import { useForm } from "react-hook-form";
+interface IFormProps {
+    username: string;
+    password: string;
+    age: number;
+}
 const FormComponent = () => {
-    interface IFormProps {
-        username: string;
-        password: string;
-    }
 
-    const [formState, setFormState] = useState<IFormProps>({
-        username: 'foobar',
-        password: '1111'
-    })
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        let user={
-            username: formState.username,
-            password: formState.password
-        };
-        console.log(user);
+
+    const {handleSubmit,register} = useForm<IFormProps>();//деструктуруємо та витягаємо методи з хука, метод handleSubmit повинен приймати колбек ф-цію, метод register автоматично створює об'єкти з назвою полів інпуту
+
+
+``
+    const customHandler = (formDataProps:IFormProps)=>{
+        console.log(formDataProps);
     };
-
-    // const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, username: input.value}); // відбувається контроль елементу Інпут (1 спосіб)
-    // };
-    // const handlePasswordChange = (e: FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, password: input.value}); // спочатку ...formState - приймає дефолтне значення, а потім за допомогою наступного елементу password: в useStste прописується з поля інпуту (те що вводимо)
-    // };
-    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
-            const input = e.target as HTMLInputElement;
-            console.log(input.name);
-            setFormState({...formState, [input.name]: input.value}); // 2-спосіб використовуємо динамічний ключ [input.name] - тобто з яким полем ми будемо працювати таке і буде підствляти
-        };
-
     return (
+
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
-                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <input type="number" {...register('age')}/>
                 <button type="submit">Send</button>
             </form>
         </div>
