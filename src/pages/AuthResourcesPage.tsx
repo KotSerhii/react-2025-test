9634c265e9ec747434106dba4b546db7fb20305f
KotSerhii <kot.serhii@gmail.com>
2025-12-15 00:17:06 +0200

    lesson 6 code complete

--- src/pages/AuthResourcesPage.tsx
+++ src/pages/AuthResourcesPage.tsx
@@ -1,9 +1,23 @@
-
+import {useEffect} from "react";
+import {loadAuthProducts, refreshToken} from "../services/api.service.tsx";
 
 const AuthResourcesPage = () => {
+
+    useEffect(() => {
+        loadAuthProducts().then(products => {
+            console.log(products)
+        })
+            .catch(reason => {
+                console.log(reason);
+                refreshToken()
+                    .then(() => loadAuthProducts())
+                    .then(products => console.log(products));
+            })
+    }, []);
+
     return (
         <div>
-            Resourses Page
+            Resources Page
         </div>
     );
 };
--- src/services/api.service.tsx
+++ src/services/api.service.tsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 import type {LoginData} from "../models/LoginData.ts";
 import type {IUserWithTokens} from "../models/IUserWithTokens.ts";
+import type {IProduct} from "../models/IProduct.ts";
+import type {IProductsResponseModelType} from "../models/IProductsResponseModelType.ts";
+import {retrieveLocalStorage} from "./helpers.tsx";
+import type {ITokenPair} from "../models/ITokenPair.ts";
 
 
 const axiosInstance = axios.create({
@@ -8,7 +12,14 @@ const axiosInstance = axios.create({
     headers: {}
 })
 
-export const login = async ({username, password, expiresInMins}: LoginData):Promise<IUserWithTokens> => {
+axiosInstance.interceptors.request.use((requestObject) => {
+    if (requestObject.method?.toUpperCase() === 'GET') {
+        requestObject.headers.Authorization = 'Bearer ' + retrieveLocalStorage<IUserWithTokens>('user').accessToken;
+    }
+       return requestObject;
+})
+
+export const login = async ({username, password, expiresInMins}: LoginData): Promise<IUserWithTokens> => {
     const {data: userWithTokens} = await axiosInstance.post<IUserWithTokens>('/login', {
         username,
         password,
@@ -17,4 +28,17 @@ export const login = async ({username, password, expiresInMins}: LoginData):Prom
     console.log(userWithTokens);
     localStorage.setItem("user", JSON.stringify(userWithTokens));
     return userWithTokens;
+}
+export const loadAuthProducts = async (): Promise<IProduct[]> => {
+    const {data: {products}} = await axiosInstance.get<IProductsResponseModelType>('/products');
+    return products;
+}
+
+export const refreshToken = async () => {
+    const IUserWithTokens=retrieveLocalStorage<IUserWithTokens>("user");
+   const {data:{accessToken, refreshToken}} =  await axiosInstance.post<ITokenPair>('/refresh',
+       {refreshToken: IUserWithTokens.refreshToken, expiresInMins:1})
+    IUserWithTokens.refreshToken=refreshToken;
+    IUserWithTokens.accessToken=accessToken;
+    localStorage.setItem("user", JSON.stringify(IUserWithTokens));
 }
