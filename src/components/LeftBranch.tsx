d5ebf8b7c9bd900da74b48fd78df64b97c12ff8f
KotSerhii <kot.serhii@gmail.com>
2026-01-16 13:14:39 +0200

    lesson 7 how to use Memo with Context

--- src/components/LeftBranch.tsx
+++ src/components/LeftBranch.tsx
@@ -1,6 +1,13 @@
 import {LeftBranchA} from "./LeftBranchA.tsx";
+import {useMemo} from "react";
 
 export const LeftBranch = () => {
+const memoCounter=useMemo(()=>{for(let i=0; i < 1000; i++) {
+        console.log(i);
+    }
+    return null},[])
+//застосовуємо useMemo, щоб при відпрацюванні Context не будувались зайвий раз 1000 ітерацій (в нашому випадку),
+// це раціональне використання хука Memo
     return (
         <div>LeftBranch
         <LeftBranchA/>
