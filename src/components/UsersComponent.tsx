34855f3ede933b4c652efd3504f685e9ccffbd39
KotSerhii <kot.serhii@gmail.com>
2026-01-27 14:14:58 +0200

    lesson 7 hook useCallback()

--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,9 +1,14 @@
 import UserComponent from "./UserComponent.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 
 const UsersComponent = () => {
     const [users, setUsers] = useState([])
+// використання хука useCallback(огортаємо ф-цію), додаємо deps.[], в цьому випадку ререндерінг компонента UserComponent, в
+    // який передаємо foo, не відбувається
+    const foo = useCallback(()=>{
+        console.log("foo");
+    },[]);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
         .then((users) => users.json())
@@ -12,7 +17,7 @@ const UsersComponent = () => {
     console.log("Users");
     return (
         <div>
-            <UserComponent/>
+            <UserComponent foo={foo}/>
         </div>
     );
 };
